8691fbfc7681e656fb2432cb06510b1848caec47
Jakub Rabęcki <kuba.rabecki@geeksdeck.net>
2019-10-17 09:29:57 +0200

    Logika rozpoczęcia gry

---
+++ resources/scripts/main.js
@@ -0,0 +1,7 @@
+const startGameButton = document.getElementById('startGameButton');
+
+startGameButton.addEventListener('click', startGame);
+
+function startGame() {
+    console.log('startGame')
+}
