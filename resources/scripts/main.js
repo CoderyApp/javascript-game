32e461363eb91c0014864acde46da34e007e1ba9
Jakub Rabęcki <kuba.rabecki@geeksdeck.net>
2019-10-17 09:32:36 +0200

    Pokazywanie i ukrywanie ninjy cz. 2

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,7 +1,17 @@
 const startGameButton = document.getElementById('startGameButton');
+const ninjas = document.querySelectorAll('.game__ninja');
 
 startGameButton.addEventListener('click', startGame);
 
 function startGame() {
-    console.log('startGame')
+    showRandomNinja();
+}
+
+function showRandomNinja() {
+    const ninja = ninjas[0]
+    ninja.classList.add('up');
+    const randomTime = 1000
+    setTimeout(() => {
+        ninja.classList.remove('up');
+    }, randomTime);
 }
